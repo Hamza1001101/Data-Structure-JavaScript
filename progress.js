09dc38280d019dac76dbf82cde660a4310dccd5d
Hamza Hayd <dado.org@hotmail.com>
2021-03-29 08:38:30 +0200

    changed some content

--- progress.js
+++ progress.js
@@ -1,67 +1,70 @@
-class Node {
+console.log("hello");
+
+class Node{
   constructor(value) {
-    this.value = value;
-    this.next = null;
+    this.node = value
+    this.next = null
   }
 }
 
 class LinkedList {
   constructor() {
-    this.tail = null;
-    this.head = null;
-    this.length = 0;
+    this.tail = null
+    this.head = null
+    this.length=0
   }
   isEmpty() {
-    return !this.length;
+    return !this.length
   }
   push(item) {
-    const newNode = new Node(item);
+    const newNode = new Node(item)
     if (this.isEmpty()) {
-      this.tail = newNode;
-      this.head = newNode;
-    } else if (this.length >= 1) {
-      this.tail.next = newNode;
-      this.tail = newNode;
+      this.head = newNode
+      this.tail = newNode
+      this.length++
     }
-    this.length++;
+    else {
+      this.tail.next = newNode
+      this.tail = newNode
+      this.length++
+    }
+    return newNode
   }
   pop() {
-    if (this.isEmpty()) {
-      return null;
+    if (this.isEmpty()) return null
+    else if (this.length === 1) {
+      const nodeToRemove = this.head
+      this.head = null
+      this.tail = null
+      this.length--
+      return nodeToRemove
     }
-    if (this.length === 1) {
-      const nodeToRemove = this.head;
-      this.head = null;
-      this.tail = null;
-      this.length--;
-      return nodeToRemove;
-    } else {
-      const nodeToRemove = this.tail;
-      let current = this.head;
-      let secondToLast;
+    else {
+      const nodeToRemove = this.tail
+      let current = this.head
+      let secondToRemove
       while (current) {
         if (current.next === this.tail) {
-          secondToLast = current
-          break;
+          secondToRemove = current
+          break
         }
-        current = current.next;
+        current = current.next
       }
-      secondToLast.next = null;
-      this.tail = secondToLast;
-      this.length--;
-      return nodeToRemove;
+      secondToRemove.next = null
+      this.tail = secondToRemove
+      this.length--
+      return nodeToRemove
     }
   }
   get(index) {
     if (index < 0 || index > this.length || this.isEmpty()) {
       return null 
     }
-    if (index === 1) return this.head
+    if (index === 0) return this.head
     if (index === this.length - 1) return this.tail
-    
     else {
-      let iterator = 0
       let current = this.head
+      let iterator =0
       while (iterator < index) {
         iterator++
         current = current.next
@@ -71,14 +74,57 @@ class LinkedList {
   }
 }
 
-const ll = new LinkedList();
-ll.push("Lunch");
-ll.push("brakfastr");
-ll.push("dinner");
-ll.push("protien");
-console.log(ll.get(2));
 
+const ll = new LinkedList()
+ll.push('tea')
+ll.push('coffee')
+ll.push('water')
+ll.push("MOBILE");
+ll.push("computer");
+//const lastItem = ll.pop()
 
-/**
- * * Bubble sort
- */
+console.log(ll.get(3))
+
+class Queue {
+  constructor() {
+    this.first = null
+    this.last = null
+    this.size=0
+  }
+  isEmpty() {
+    return !this.size
+  }
+  enqueue(item) {
+    let newNode = new Node(item)
+    if (this.isEmpty()) {
+      this.first = newNode
+      this.last = newNode
+    }
+    else {
+      this.last.next = newNode
+      this.last = newNode
+    }
+    return this.size++
+ 
+  }
+  dequeue() {
+    if (!this.size) return null
+    if (this.first === this.last) {
+      this.last= null 
+    }
+    this.first = this.first.next
+    this.size--
+  }
+  peek() {
+    return this.first
+  }
+}
+
+const qq = new Queue()
+qq.enqueue('dollar')
+qq.enqueue('Euro')
+qq.enqueue('somali shilling')
+qq.dequeue()
+qq.dequeue()
+qq.dequeue();
+console.log(qq.peek())

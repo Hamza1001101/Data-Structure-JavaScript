579aafd06ac6b654c016d869043c2211c21f0072
Hamza Hayd <dado.org@hotmail.com>
2021-04-05 20:19:26 +0200

    added a enqueu function

--- progress.js
+++ progress.js
@@ -10,4 +10,19 @@ class Queue {
     this.last = null;
     this.size = 0;
   }
+  //isEmpty
+  isEmpty() {
+    return !this.size;
+  }
+  //enqueu
+  enqueu(item) {
+    const newNode = new Node(item);
+    if (this.isEmpty()) {
+      this.first = newNode;
+      this.last = newNode;
+    }
+    this.last.next = newNode;
+    this.last = newNode;
+    this.size++;
+  }
 }

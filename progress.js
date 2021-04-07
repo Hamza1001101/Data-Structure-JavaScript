7e77157beeca19e12fbf623954a2ddbdd6cc5430
Hamza Hayd <dado.org@hotmail.com>
2021-04-07 08:56:08 +0200

    added a push function

--- progress.js
+++ progress.js
@@ -10,4 +10,18 @@ class Stack {
     this.bottom = null;
     this.size = 0;
   }
+  isEmpty() {
+    return !this.size;
+  }
+  push(item) {
+    const newNode = new Node(item);
+    if (this.isEmpty()) {
+      this.bottom = newNode;
+      this.top = newNode;
+    }
+    const holderPointer = this.last;
+    this.last = newNode;
+    this.last.next = holderPointer;
+    this.size++;
+  }
 }

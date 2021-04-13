9657888b1ed05d89e607f4db4f9b60b395f436bf
Hamza Hayd <dado.org@hotmail.com>
2021-04-13 10:56:13 +0200

    implemented Quick sort

--- sortAlgorithms.js
+++ sortAlgorithms.js
@@ -31,3 +31,19 @@ console.log(mergeSort(arr));
  * * Quick sort  O(n log n)
  */
 
+const quickSort = (arr) => {
+  if (arr.length < 2) return arr;
+  let pivotIndex = Math.floor(arr.length / 2);
+  let pivot = arr[pivotIndex];
+  let less = [];
+  let greater = [];
+  for (let i in arr) {
+    if (i !== pivotIndex) {
+      arr[i] > pivot ? greater.push(arr[i]) : less.push(arr[i]);
+    }
+  }
+  return [...quickSort(less), pivot, ...quickSort(greater)];
+};
+
+console.log("*************");
+console.log(quickSort(arr));

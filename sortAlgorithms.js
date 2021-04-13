2423fdf8cdebeb46d9ce685dd50a6b9ebf0ff187
Hamza Hayd <dado.org@hotmail.com>
2021-04-13 10:36:08 +0200

    implemented Mergesort

--- sortAlgorithms.js
+++ sortAlgorithms.js
@@ -2,21 +2,27 @@
  * * Bubble Sort
  */
 
-const mergeSort = (arr) => {
-  if (arr.length < 2) return arr;
+function mergeSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
   const middle = Math.floor(arr.length / 2);
   const left = arr.slice(0, middle);
   const right = arr.slice(middle);
   return merge(mergeSort(left), mergeSort(right));
-};
-
-const merge = (left, right) => {
+}
+function merge(left, right) {
   const sorted = [];
   while (left.length && right.length) {
     if (left[0] <= right[0]) {
-      sorted.push(left.shift);
-    } else sorted.push(right.shift());
+      sorted.push(left.shift());
+    } else {
+      sorted.push(right.shift());
+    }
   }
-  let result = [...sorted, ...left, ...right];
-  return result;
-};
+  let results = [...sorted, ...left, ...right];
+  return results;
+}
+const arr = [3, 52, 1, 432, 1, 32, -4, 2];
+
+console.log(mergeSort(arr));
